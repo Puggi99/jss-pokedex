73c3234d89894bc82710f0a97da6de23cb2cc1b3
Puggi99 <127298032+Puggi99@users.noreply.github.com>
2023-04-27 19:06:10 +0200

    dd

--- detail.js
+++ detail.js
@@ -46,7 +46,55 @@ PokeService.getDetail(pokemonName).then(pokemonObject => {
 
 
 function displayPokemon(pokemon){
-   const title = document.getElementById('pokemonName-container')
-   title.appendChild(pokemon)
+    // const pokemonContainer = document.getElementById('pokemonName-container');
+    // pokemonContainer.innerHTML =``;
+    // const pokemonNode = document.createTextNode(`${pokemon}, ${pokemon.stats}`);
+    // pokemonContainer.appendChild(pokemonNode);
 }
+
+
+
+
+PokeService.getDetail(pokemonName).then(pokemon=>{
+    displayPokemonName(pokemon);
+    displayPokemonHeight(pokemon)
+    displayPokemonWeight(pokemon)
+})
+
+    
+
+
+function displayPokemonName(pokemon){
+    const pokemonName = document.getElementById('pokemonName-container');
+    pokemonName.innerHTML =``;
+    const pokemonText = document.createTextNode(`${pokemon.name}`);
+    pokemonName.appendChild(pokemonText);
+}
+
+function displayPokemonHeight(pokemon){
+    const pokemonHeight = document.getElementById('pokemon-height');
+    pokemonHeight.innerHTML = ``;
+    pokemonHeight.innerHTML += 'Height: ' + pokemon.height*10 + 'cm'
+}
+
+
+function displayPokemonWeight(pokemon){
+    const pokemonWeight = document.getElementById('pokemon-weight');
+    pokemonWeight.innerHTML = ``;
+    pokemonWeight.innerHTML += 'Weight: ' + pokemon.weight/10 + 'kg'
+}
+
+
+// function displayPokemonStats(pokemon){
+//     const pokemonAbilities = document.getElementById('pokemon-abilities')
+//     pokemonAbilities.innerHTML = ''
+    
+
+
+//     for (let i = 0; i < pokemon.abilities.length; i++) {
+//         const abilityObject = pokemon.abilities[i];
+//         pokemon.addAbility(abilityObject.ability.name, abilityObject.ability.url)
+        
+//     }
+// }
 
